e538bc40ec09db859a2687bc84b5b9f20158b908
Jayla Bullard <jaylabullard@Jaylas-Air.attlocal.net>
2021-02-09 12:04:58 -0500

    Second commit: finished.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,33 @@
 
 namespace MoneyMaker
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //CAPTURING INPUT
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Enter amount to convert to coins: ");
+            string totalAsString = Console.ReadLine();
+
+            double totalAsDouble = Convert.ToDouble(totalAsString);
+
+            Console.WriteLine($"You are converting {totalAsDouble} cents to: ");
+
+            //CONVERT TO COINS
+            int goldCoinValue = 10;
+            int silverCoinValue = 5;
+
+            double goldCoin = Math.Floor(totalAsDouble / goldCoinValue);
+            double remainder = totalAsDouble % 10;
+
+            double silverCoin = Math.Floor(remainder / silverCoinValue);
+
+            remainder = remainder % silverCoinValue;
+
+            Console.WriteLine($"Gold coins: {goldCoin}\nSilver coins: {silverCoin}\n");
+
         }
     }
 }
