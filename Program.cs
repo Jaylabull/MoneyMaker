ae0759fa842aacaa2c78bf9a0f89ee6b1bfa8ce1
Jayla Bullard <jaylabullard@Jaylas-Air.attlocal.net>
2021-02-09 12:03:31 -0500

    Second commit: finished.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
